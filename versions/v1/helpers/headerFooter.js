4d0a8fbb6dd091b7e64ffe44fb45079037ac94e5
Robbie106gti <rleeuwerink@gmail.com>
2019-05-13 15:28:31 -0700

    product line in GI and new header footer

--- versions/v1/helpers/headerFooter.js
+++ versions/v1/helpers/headerFooter.js
@@ -1,22 +1,18 @@
 function headerFooter(location) {
-  // console.log(location);
-  if (location === null) {
-    location = '../../../';
-  }
-  var header = location + 'layout/header.html';
-  var footer = location + 'layout/footer.html';
-  $.ajax({
-    url: header,
-    context: document.body,
-    success: function(response) {
-      $('#header').html(response);
-    }
-  });
-  $.ajax({
-    url: footer,
-    context: document.body,
-    success: function(response) {
-      $('#footer').html(response);
-    }
-  });
+  var header = '<nav>'.concat('<div class="nav-wrapper brown darken-3">', '<div class="whiteLine"></div>', '<ul class="brand-logo">', '<li><a href="/catalog/build/"><img class="imageLogo" src="/catalog/build/assets/logoNC.png" alt="Nickels Cabinets" /></a></li>', '<li><span class="headingLogo"><i class="material-icons">collections_bookmark</i> Catalog</span></li>', '<li class=""><button id="refresh" class="btn-small open brown-text hide"><i class="material-icons">refresh</i></button></li>', '<li><small id="prodline" class="headingLogo"></small></li>', '</ul>', '</div><a id="TopPage"></a></nav>');
+  document.getElementById('header').innerHTML = header;
+  var footer = '<div id="dadjoke"></div>'.concat('<footer class="page-footer brown darken-3">', '<div class="footer-copyright">', '<div class="container">', '© 2019 Copyright Nickels Cabinets', '<a class="grey-text text-lighten-4 right" href="/catalog/build/">Start Catalog</a>', '</div>', '</div>', '<a id="BottomPage"></a></footer>');
+  document.getElementById('footer').innerHTML = footer;
+
+  setLine()
+  var refreshBtn = document.getElementById('refresh');
+  var myIframe = parent.document.getElementById('pgCatalog');
+  var myIframe2 = parent.document.getElementById('nklcatalog');
+  if (window.location !== window.parent.location) refreshBtn.classList.remove('hide');
+
+  refreshBtn.onclick = function () {
+    myIframe.src === null ? null : myIframe.contentWindow.location.reload(true);
+    myIframe2.src === null ? null : myIframe2.contentWindow.location.reload(true);
+  };
 }
+
