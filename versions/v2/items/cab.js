648db3cc0c46a28968903602bb1dbe4697864f53
Robbie106gti <rleeuwerink@gmail.com>
2019-07-10 14:43:52 -0700

    New Form x door and update sink images

---
+++ versions/v2/items/cab.js
@@ -0,0 +1,142 @@
+//// Page js for cabinet pages /////
+headerFooter('../');
+
+window.onload = getPage();
+if ((pline = undefined)) {
+  if (window.location !== window.parent.location) {
+    setPline();
+  }
+}
+
+function getPage() {
+  makeStructure();
+  fetch('../versions/v2/json/codes.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var codes = data['codes'];
+      var page = codes.filter(function (el) {
+        var res = code.search(el.root);
+        var arr = new Array();
+        arr.push(el.root + '__' + el.height);
+        arr.push(el.code);
+        if (res !== -1) {
+          el.height
+            ? el.widths.map(function (h) {
+              return arr.push(el.root + h + el.height);
+            })
+            : '';
+        }
+        return arr.includes(code);
+      });
+      page = page[0];
+
+      setGI(page);
+      setCode(page);
+    })
+    .then(function () {
+      lastCallCodes();
+      $('.materialboxed').materialbox();
+    })
+    .catch(function (err) {
+      return console.log(err);
+    });
+}
+
+function makeStructure() {
+  var main =
+    '<div class="col s12 m6"><div id="des"></div><div id="specs"><div class="card-panel grey lighten-3 bullet"><span class="card-title"><h4>Specifications</h4></span><div class="divider"></div><div id="specli"></div></div></div><div id="notes"></div></div><div class="col s12 m6"><div id="images"></div></div><div class="col s12 m12"><div id="codes" class="col s12 m6"></div><div id="options" class="col s12 m6"></div></div>';
+  $('#catalog').html(main);
+}
+
+function setCode(page) {
+  disableItem(page);
+  description(page.title, page.description);
+  setSpecs(page.specifications, page);
+  codeTable(page);
+  setImages(page.images, page.title, page.height);
+  setNotes('../', page.notes);
+  setActions(page);
+}
+
+function setActions(page) {
+  var cat = page.cat.toLowerCase();
+  fetch('../versions/v1/json/'.concat(cat, '.json'))
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var card = data[page.cat].filter(function (el, i) {
+        return el.code === page.root;
+      });
+      card = card[0];
+
+      var action = "<!-- Dropdown Trigger --><a class='dropdown-button btn bot light-blue darken-4' data-activates='dropdown1'>Other options</a><!-- Dropdown Structure --><ul id='dropdown1' class='dropdown-content light-blue darken-4'>".concat(
+        makeActions(card),
+        '</ul>'
+      );
+      $('#actions').html(action);
+      $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: true, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'left', // Displays dropdown with edge aligned to the left of button
+        stopPropagation: false // Stops event propagation
+      });
+    })
+    .catch(function (err) {
+      return console.log(err);
+    });
+}
+function makeActions(card) {
+  // console.log(card);
+  var action = card.attached
+    .map(function (a) {
+      // console.log(a);
+      if (skipItem(a) === true) {
+        return '<li class="waves-effect waves-light"><a class="white-text" href="./'
+          .concat(a.link, '.html?code=')
+          .concat(card.code)
+          .concat(a.height, '"><i class="material-icons">art_track</i>')
+          .concat(a.height, '" high</a></li>');
+      } else {
+        return '';
+      }
+    })
+    .join('');
+  return action;
+}
+
+function additional(page) {
+  if (!page.additional) {
+    return '';
+  }
+
+  var add = '<br><div class="divider"></div><br><table class="striped highlight centered"><thead><tr>'
+    .concat(
+      page.additional.header
+        .map(function (h) {
+          return '<th>'.concat(h, '</th>');
+        })
+        .join(''),
+      '</tr></thead><tbody id="tbody">'
+    )
+    .concat(
+      page.additional.rows
+        .map(function (r) {
+          return '<tr><td>'
+            .concat(r.cw, '</td><td>')
+            .concat(r.fw, '</td><td>')
+            .concat(r.l, '</td><td>')
+            .concat(r.dw, '</td></tr>');
+        })
+        .join(''),
+      '</tbody></table>'
+    )
+    .concat(addnotes(page.additional));
+  return add;
+}
---
+++ versions/v2/items/fillers.js
@@ -0,0 +1,66 @@
+//// Main js for catagories /////
+headerFooter(null);
+window.onload = getPage();
+info.cat = 'Fillers';
+var catalog = document.getElementById('catalog');
+function getPage() {
+  fetch('../../../versions/v2/items/fillers.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (!info.code) {
+        var tabs = data['sub-cats'];
+        var html = tabs
+          .map(function (t) {
+            return cardWith(t);
+          })
+          .join('');
+        console.log(catalog);
+
+        if (edge === '') catalog.classList.add('grid');
+        catalog.innerHTML = html;
+        setGIA(info);
+        return;
+      } else {
+        info.item = data.items.filter(function (item) {
+          return item.code.toLowerCase() === code;
+        })[0];
+      }
+      makeStructure(info);
+    }).then(function () {
+      lastCallCodes();
+      $('.materialboxed').materialbox();
+    })
+    .catch(function (err) {
+      return console.log(err);
+    });
+}
+
+function makeStructure(info) {
+  setGIA(info);
+  var structure = ''.concat(
+    '<div class="col s12 m8">',
+    '<div id="des"></div>',
+    '<div id="div-sor" class="card-panel hide">',
+    '<div id="spec"></div>',
+    '<div id="options"></div>',
+    '<div id="restrictions"></div>',
+    '</div>',
+    '<div id="notes"></div>',
+    '<div id="codes"></div>',
+    '</div>',
+    '<div id="images" class="col s12 m4">',
+    setMainImage(info.item),
+    '</div><div id="table" class="col s4"></div>');
+
+  $('#catalog').html(structure);
+  setSpecsND('../../..', info.item.specifications);
+  if (info.item.options) optionSpecs('../../..', info.item.options);
+  if (info.item.restrictions)
+    restrictionsSpecs('../../..', info.item.restrictions);
+  $('#table').html('<div class="card-panel">' + simpleTable(info) + '</div>');
+  description(info.item.title, info.item.description);
+  // $('#codes').html(setCode(info.item.code));
+  setNotes('../../../', info.item.notes);
+}
