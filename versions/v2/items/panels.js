22f8b921e44257f31caa9d4fab08d4d0bb0a37c2
Robbie106gti <rleeuwerink@gmail.com>
2019-07-10 09:30:26 -0700

    width panels note doors

---
+++ versions/v2/items/panels.js
@@ -0,0 +1,70 @@
+//// Main js for catagories /////
+headerFooter(null);
+window.onload = getPage();
+info.cat = 'Panels';
+var catalog = document.getElementById('catalog');
+function getPage() {
+  fetch('../../../versions/v2/items/panels.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (!info.code) {
+        var tabs = data['sub-cats'];
+        var html = tabs
+          .map(function (t) {
+            return cardWith(t);
+          })
+          .join('');
+        // console.log(catalog);
+
+        if (edge === '') catalog.classList.add('grid');
+        catalog.innerHTML = html;
+        setGIA(info);
+        return;
+      } else {
+        info.item = data.items.filter(function (item) {
+          return item.code.toLowerCase() === code;
+        })[0];
+      }
+      makeStructure(info);
+    }).then(function () {
+      lastCallCodes();
+      $('.materialboxed').materialbox();
+    })
+    .catch(function (err) {
+      return console.log(err);
+    });
+}
+
+function makeStructure(info) {
+  setGIA(info);
+  // console.log(info)
+  var structure = ''.concat(
+    '<div class="col s12 m8">',
+    '<div id="des"></div>',
+    '<div id="div-sor" class="card-panel hide">',
+    '<div id="spec"></div>',
+    '<div id="options"></div>',
+    '<div id="restrictions"></div>',
+    '</div>',
+    '<div id="notes"></div>',
+    '<div id="codes"></div>',
+    '</div>',
+    '<div id="images" class="col s12 m4">',
+    setMainImage(info.item),
+    '</div><div id="table" class="col s4"></div>');
+
+
+  // '<div class="col s12 m8"><div id="des"></div><div id="div-sor" class="card-panel hide"><div id="spec"></div><div id="options"></div><div id="restrictions"></div><div id="notes"></div><div id="codes"></div></div><div id="images" class="col s12 m4">' +
+  // setMainImage(info.item) +
+  // '</div><div id="table" class="col s4"></div>';
+  $('#catalog').html(structure);
+  setSpecsND('../../..', info.item.specifications);
+  if (info.item.options) optionSpecs('../../..', info.item.options);
+  if (info.item.restrictions)
+    restrictionsSpecs('../../..', info.item.restrictions);
+  $('#table').html('<div class="card-panel">' + simpleTable(info) + '</div>');
+  description(info.item.title, info.item.description);
+  setNotes('../../../', info.item.notes);
+}
