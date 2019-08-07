21e92c3fd8c7a8d2ae40ca57e7886ec37743f80b
Robbie106gti <rleeuwerink@gmail.com>
2019-08-07 15:39:08 -0700

    regex in search

--- versions/v2/items/cab.js
+++ versions/v2/items/cab.js
@@ -17,10 +17,11 @@ function getPage() {
     .then(function (data) {
       var codes = data['codes'];
       var page = codes.filter(function (el) {
-        var res = code.search(el.root);
+        var res = code.search(/el.root/i);
         var arr = new Array();
         arr.push(el.root + '__' + el.height);
         arr.push(el.code);
+        console.log({ res, info, height: el.height, widths: el.widths, code: el, arr })
         if (res !== -1) {
           el.height
             ? el.widths.map(function (h) {
@@ -28,8 +29,10 @@ function getPage() {
             })
             : '';
         }
+        console.log(arr)
         return arr.includes(code);
       });
+      console.log(page)
       page = page[0];
 
       setGI(page);
